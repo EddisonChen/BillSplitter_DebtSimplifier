5341736a6420739839b80412fd486922a1194ec6
Eddison Chen <eddisonchen1@gmail.com>
2025-12-12 00:17:19 -0500

    finished additem functionality, calculating tip and tax. Need to create final data structure for who owes who that can be used both standalone and as part of the debt simplifier

--- billsplitter_debtsimplifier/src/Components/AddItem/AddItem.jsx
+++ billsplitter_debtsimplifier/src/Components/AddItem/AddItem.jsx
@@ -1,37 +1,74 @@
 import { useState } from "react"
+import WarningAlert from "../../MiniComponents/Alert/Alert";
 
-const AddItem = ({users, setShowPopup}) => {
-
-    console.log(users)
+const AddItem = ({users, setShowPopup, items, setItems}) => {
 
     const [involvedUsers, setInvolvedUsers] = useState([]);
-    const [user, setUser] = useState(null);
-
-    const userInputChange = () => {
-        setUser(user);
-    }
-
-    const addUserToItem = (event) => {
-        event.preventDefault();
-        if (user !== null) {
-            setInvolvedUsers(involvedUsers => [...involvedUsers, user])
+    const [itemCost, setItemCost] = useState();
+    const [itemName, setItemName] = useState();
+    const [warning, setWarning] = useState();
+    
+    const handleItemInput = (event) => {
+        if (event.target.name == "itemName") {
+            setItemName(event.target.value);
+        } else if (event.target.name == "itemCost") {
+            setItemCost(Number(event.target.value));
         }
     }
 
-    console.log(involvedUsers)
+    const addUserToItem = (event) => {
+        if (event.target.checked) {
+            setInvolvedUsers(involvedUsers => [...involvedUsers, event.target.value]);
+        } else {
+            setInvolvedUsers(involvedUsers => involvedUsers.filter(user => user !== event.target.value));
+        }
+    }
+
+    const involvedUserCheckboxes = users.map((user) => (
+        <div>
+            {user}<input type="checkbox" value={user} onChange={addUserToItem} key={user}/>
+        </div>
+    ));
+
+    const handleSubmit = () => {
+
+        const itemObject = {
+            itemName: itemName,
+            itemCost: itemCost,
+            involvedUsers: involvedUsers,
+            taxAmount: 0,
+            tipAmount: 0,
+            totalCost: 0,
+        };
+
+        setItems(items => [...items, itemObject]);
+
+        setShowPopup();
+    }
+
+    const validateInputs = (event) => {
+        event.preventDefault();
+        if (itemCost == undefined || itemCost == "" || itemName == undefined || itemName == "") {
+            setWarning("One or more fields empty!");
+        } else if (involvedUsers.length < 1) {
+            setWarning("Please select at least one user");
+        } else {
+            handleSubmit()
+        }
+        
+    }
+
     return(
         <div>
             <form>
-                <input type="text" placeholder="Item Name"></input>
-                <input type="number" placeholder="Item Cost"></input>
-                <select onChange={userInputChange}>
-                    <option disabled>-- Select User --</option>
-                    {users.map((party) => (
-                        <option value={user}>{party}</option>
-                    ))}
-                </select>
-                <button onClick={addUserToItem}>Add User to Transaction</button>
+                <input type="text" placeholder="Item Name" name="itemName" onChange={handleItemInput}></input>
+                <input type="number" placeholder="Item Cost" name="itemCost" onChange={handleItemInput}></input>
+                {involvedUserCheckboxes}
+                <button type="submit" onClick={validateInputs}>Submit Item</button>
             </form>
+            {warning && (
+                <WarningAlert message={warning} />
+            )}
         </div>
     )
 }
--- billsplitter_debtsimplifier/src/Components/BillSplitter/BillSplitter.jsx
+++ billsplitter_debtsimplifier/src/Components/BillSplitter/BillSplitter.jsx
@@ -6,6 +6,28 @@ const BillSplitter = ({parties}) => {
     const [tempParties, setTempParties] = useState([]);
     const [partyInput, setPartyInput] = useState("");
     const [showPopup, setShowPopup] = useState(false);
+    const [items, setItems] = useState([]);
+    const [payor, setPayor] = useState();
+    const [taxInputType, setTaxInputType] = useState("percentage");
+    const [taxInput, setTaxInput] = useState(0);
+    const [tipInput, setTipInput] = useState(0);
+    const [tipAfterTax, setTipAfterTax] = useState(false);
+
+    const handleTipAfterTax = () => {
+        setTipAfterTax(!tipAfterTax);
+    }
+
+    const handleTaxInputTypeSwitch = (event) => {
+        setTaxInputType(event.target.value);
+    }
+
+    const handleTaxInput = (event) => {
+        setTaxInput(Number(event.target.value));
+    }
+
+    const handleTipInput = (event) => {
+        setTipInput(Number(event.target.value)/100);
+    }
 
     if (parties !== undefined) {
         setTempParties(...parties);
@@ -27,6 +49,72 @@ const BillSplitter = ({parties}) => {
         setShowPopup(!showPopup)
     }
 
+    const updatePaymentCalculation = () => {
+
+    }
+
+    useEffect(() => {
+        updatePaymentCalculation();
+    }, [items])
+
+    const showItems = items.map((item) => (
+        <tr>
+            <td>{item.itemName}</td>
+            <td>{item.itemCost}</td>
+            <td>{item.involvedUsers}</td>
+        </tr>
+
+    ));
+
+    const handlePayor = (event) => {
+        setPayor(event.target.value);
+    }
+
+    const calculateTax = () => {
+        let taxAmountperItem;
+        const temp = [...items];
+        if (taxInputType == "percentage") {
+            for (let i=0; i<temp.length; i++) {
+                taxAmountperItem = (taxInput/100)*temp[i].itemCost;
+                temp[i].taxAmount = taxAmountperItem
+            }
+        } else if (taxInputType == "amount") {
+            let sumCost = 0;
+            for (let i=0; i<temp.length; i++) {
+                sumCost += temp[i].itemCost
+            }
+            const tipPercentage = taxInput/sumCost;
+            for (let i=0; i<temp.length; i++) {
+                taxAmountperItem = tipPercentage*temp[i].itemCost;
+                temp[i].taxAmount = taxAmountperItem
+            }
+        }
+        setItems(temp);
+    }
+
+    const calculateTip = () => {
+        const temp = [...items];
+        for (let i=0; i<temp.length; i++) {
+            if (tipAfterTax) {
+                const costPlusTip = temp[i].itemCost + temp[i].taxAmount;
+                temp[i].tipAmount = tipInput*costPlusTip;
+            } else {
+                temp[i].tipAmount = tipInput*temp[i].itemCost;
+            }
+        }
+        setItems(temp);
+    }
+
+    useEffect(() => {
+        calculateTax();
+    }, [items.length, taxInput, taxInputType]);
+
+    useEffect(() => {
+        calculateTip();
+    }, [items.length, taxInput, taxInputType, tipInput]);
+
+    console.log(items)
+
     return (
         <div>
             <div>
@@ -38,10 +126,60 @@ const BillSplitter = ({parties}) => {
                     <button type="submit" onClick={addTempParty}>Submit</button>
                 </form>
             </div> : null}
+            <select onChange={handlePayor} defaultValue="">
+                <option disabled="disabled" value="">--Select Payor--</option>
+                {tempParties.map((party) => (
+                    <option value={party} key={party}>{party}</option>
+                ))}
+            </select>
             <div>
                 <button onClick={togglePopup}>Add Item</button>
-                {showPopup && tempParties.length>0 ? <AddItem users={tempParties} showPopup={showPopup} setShowPopup={setShowPopup}/>: null}
+                {showPopup && tempParties.length>0 ? <AddItem 
+                    users={tempParties} 
+                    showPopup={showPopup} 
+                    setShowPopup={setShowPopup}
+                    items={items}
+                    setItems={setItems}
+                    />: null}
             </div>
+            <div>
+                <input type="number" placeholder="Tip %" onChange={handleTipInput}></input>
+                <input type="number" placeholder="Tax" onChange={handleTaxInput}></input>
+                <input type="checkbox" onChange={handleTipAfterTax}></input><label>Tip After Tax?</label>
+                <div>
+                    <p>Tax Input Type</p>
+                    <input type="radio" name="Tax Input Type" value="percentage" defaultChecked onChange={handleTaxInputTypeSwitch}></input><label>Percentage</label>
+                    <input type="radio" name="Tax Input Type" value="amount" onChange={handleTaxInputTypeSwitch}></input><label>Dollar Amount</label>
+                </div>            
+            </div>
+            {items.length > 0 && (
+                <div>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Item Name:</th>
+                                <th>Item Cost:</th>
+                                <th>Split Between:</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {showItems}
+                        </tbody>
+                </table>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Cost of Items</th>
+                            <th>Total Tip Amount:</th>
+                            <th>Final Cost</th>
+                        </tr>
+                    </thead>
+                    
+                </table>
+                </div>
+
+                
+            )}
             
         </div>
     )
