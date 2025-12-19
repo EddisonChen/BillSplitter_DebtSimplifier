22764e7e4af5fb48a274adcf0e891687ea022834
Eddison Chen <eddisonchen1@gmail.com>
2025-12-19 00:29:48 -0500

    fixed add parties box

--- billsplitter_debtsimplifier/src/Components/BillSplitter/SpeedDialActions/AddTempParty.jsx
+++ billsplitter_debtsimplifier/src/Components/BillSplitter/SpeedDialActions/AddTempParty.jsx
@@ -29,16 +29,18 @@ const AddTempParty = ({showModal, toggleModal, tempParties, partyInput, setParty
         setPartyInput("");
     };
 
-    const style = {
+   const style = {
         position: 'absolute',
         top: '50%',
         left: '50%',
         transform: 'translate(-50%, -50%)',
-        width: '80%',
+        width: '65%',
         bgcolor: 'background.paper',
-        border: '2px solid #000',
+        borderRadius: "15px",
         boxShadow: 24,
         p: 4,
+        overflowY: 'auto',
+        maxHeight: '85vh'
     };
 
     return (
